6df1133c989504cd8637283fc2d030a412e83d08
Farhan Kaioum <km1472233@gmail.com>
2020-11-07 00:46:53 +0600

    docs ts added

---
+++ WebApplication/wwwroot/ts/docs.js
@@ -0,0 +1,6 @@
+function createSquare(config) {
+    return { color: config.color || "red", area: config.width ? config.width * config.width : 20 };
+}
+var squareOptions = { colour: "red" };
+var mySquare = createSquare(squareOptions);
+//# sourceMappingURL=docs.js.map
---
+++ WebApplication/wwwroot/ts/docs.ts
@@ -0,0 +1,12 @@
+﻿interface SquareConfig {
+    color?: string;
+    width?: number;
+    [propName: string]: any;
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+    return { color: config.color || "red", area: config.width ? config.width * config.width : 20 };
+}
+
+let squareOptions = { colour: "red" };
+let mySquare = createSquare(squareOptions);
